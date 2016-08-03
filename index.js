fb7d83064569a3f8a10313b0f977812687320513
DevinFlick <DevinJFlick@gmail.com>
2016-08-03 19:36:46 -0400

    created comments router

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var server = express();
-var bodyParser = require(body-parser);
+var bodyParser = require('body-parser');
 var cors = require('cors');
+var commentRouter = require('./routes/comments.js'); //injects middleware we just made into this server
 
 var port = process.env.PORT || 8080;
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended:true}));
 server.use(cors());
+server.use(commentRouter);
 
 server.get('/', function(req, res){
   res.send('HEEEEEEEEEEEEEY');
