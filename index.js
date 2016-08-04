f721bd06efd38b95cae6a9c9c9f9198d6597b0af
DevinFlick <DevinJFlick@gmail.com>
2016-08-03 21:00:07 -0400

    routes to mongodb working

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ var server = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var commentRouter = require('./routes/comments.js'); //injects middleware we just made into this server
+var postRouter = require('./routes/posts.js');
+var mongoose = require('mongoose');
 
 var port = process.env.PORT || 8080;
+var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
 
+mongoose.connect(mongoURI);
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended:true}));
 server.use(cors());
 server.use(commentRouter);
+server.use(postRouter);
 
 server.get('/', function(req, res){
   res.send('HEEEEEEEEEEEEEY');
--- routes/posts.js
+++ routes/posts.js
@@ -1,2 +1,48 @@
 var express = require ('express');
-var router = 
+var router = express.Router();
+var Post = require('../models/post.js');
+
+router.get('/posts', getPosts);
+router.post('/posts', createPost);
+router.get('/posts/:postId', getAPost);
+router.delete('/posts/:postId', deleteAPost);
+router.put('/posts/:postId', updateAPost);
+
+module.exports = router;
+
+function getPosts(req, res, next){
+  console.log('getting all the Posts');
+  next();
+};
+function createPost(req, res, next){
+  var post = new Post({
+    title: req.body.title,
+    author: req.body.author,
+    body: req.body.body,
+    created: new Date(),
+    updated: new Date(),
+  });
+  post.save(function(err,newPost){
+    if (err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(201).json({
+        post: newPost
+      });
+    }
+  });
+};
+function getAPost(req, res, next){
+  console.log('get a certain Post');
+  next();
+};
+function deleteAPost(req, res, next){
+  console.log('delete a certain Post');
+  next();
+};
+function updateAPost(req, res, next){
+  console.log('update a certain Post');
+  next();
+};
