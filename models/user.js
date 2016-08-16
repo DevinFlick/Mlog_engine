2bb7aef9f74372945b0e9643d8352015a433d012
DevinFlick <DevinJFlick@gmail.com>
2016-08-16 19:03:44 -0400

    added password validation using salt and hash

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var crypto = require('crypto'); // already in node no install required
+
 
 var userSchema = new Schema ({
   firstName: {
@@ -22,7 +24,31 @@ var userSchema = new Schema ({
   signedUp:{
     type: Date,
   },
+  hash:{
+    type: String,
+    required: false,
+  },
+  salt:{
+    type: String,
+    required: false,
+  },
 });
+
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+                    .toString('hex');
+};
+
+userSchema.methods.validPassword = function(password){
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+                    .toString('hex');
+  return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function(){
+
+};
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;
